cba54e2a546acf7a86e64ba7a7a3989c593366df
bogatovnikita <bogatovnikita@gmail.com>
2021-12-30 10:35:28 +0300

    lesson #03. Add list world and russian city, edit repo

--- app/src/main/java/com/bogatovnikita/myweather/model/Repository.kt
+++ app/src/main/java/com/bogatovnikita/myweather/model/Repository.kt
@@ -2,5 +2,6 @@ package com.bogatovnikita.myweather.model
 
 interface Repository {
     fun getWeatherFromServer(): Weather
-    fun getWeatherFromLocalStorage(): Weather
+    fun getWeatherFromLocalStorageRus(): List<Weather>
+    fun getWeatherFromLocalStorageWorld(): List<Weather>
 }
--- app/src/main/java/com/bogatovnikita/myweather/model/RepositoryImplemented.kt
+++ app/src/main/java/com/bogatovnikita/myweather/model/RepositoryImplemented.kt
@@ -5,7 +5,11 @@ class RepositoryImplemented : Repository {
         return Weather()
     }
 
-    override fun getWeatherFromLocalStorage(): Weather {
-        return Weather()
+    override fun getWeatherFromLocalStorageRus(): List<Weather> {
+        return getRussianCities()
+    }
+
+    override fun getWeatherFromLocalStorageWorld(): List<Weather> {
+        return getWorldCities()
     }
 }
