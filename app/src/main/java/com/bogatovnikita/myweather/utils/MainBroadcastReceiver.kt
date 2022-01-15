3dfc2357daab2bbcc4f36bf1bc54e95586fa17ad
bogatovnikita <GfiFhj57?,;)&gjJv57!>
2022-01-15 16:00:22 +0300

    Lesson#06. Init commit and Add MainBroadcastReceiver.kt

---
+++ app/src/main/java/com/bogatovnikita/myweather/utils/MainBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.bogatovnikita.myweather.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MainBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
